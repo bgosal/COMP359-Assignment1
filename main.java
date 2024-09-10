e8faa8c22a0accbf9a4f6fcf33d2b520f615cf00
Bhupinder Gosal (300140608) <bhupinder.gosal@student.ufv.ca>
2024-09-10 15:41:14 -0700

    setup of the main functions

--- main.java
+++ main.java
@@ -1,3 +1,33 @@
 public class main{
-    
+
+    public void quicksort(){
+        
+          
+
+    }  
+
+
+    public int partition(){
+        return 0;
+        
+    }
+
+    public int choosePivot(){
+        return 0;
+
+    }
+
+    public void swap(){
+
+    }
+
+
+
+
+    public static void main(String[] args){
+
+		
+
+	}
+
 }
